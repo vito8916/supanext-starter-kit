8267bf3a553d2ecd3dcd6bbc2d14e084e53cb09e
Victor Alvarado <vito8916@gmail.com>
2025-03-03 15:16:35 -0600

    Refactor user menu component to simplify logout and settings navigation

--- components/dashboard/user-menu-content.tsx
+++ components/dashboard/user-menu-content.tsx
@@ -1,40 +1,42 @@
-import { DropdownMenuGroup, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from '@/components/ui/dropdown-menu';
-import { UserInfo } from '@/components/dashboard/user-info';
-import Link from 'next/link';
-import { LogOut, Settings } from 'lucide-react';
-import { signOutAction } from '@/app/actions';
+import {
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+} from "@/components/ui/dropdown-menu";
+import { UserInfo } from "@/components/dashboard/user-info";
+import Link from "next/link";
+import { LogOut, Settings } from "lucide-react";
+import { signOutAction } from "@/app/actions";
 
 interface User {
-    name: string;
-    email: string;
-    avatar: string;
+  name: string;
+  email: string;
+  avatar: string;
 }
 
-export function UserMenuContent({user}: {user: User}) {
-
-    return (
-        <>
-            <DropdownMenuLabel className="p-0 font-normal">
-                <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
-                    <UserInfo user={user} showEmail={true} />
-                </div>
-            </DropdownMenuLabel>
-            <DropdownMenuSeparator />
-            <DropdownMenuGroup>
-                <DropdownMenuItem asChild>
-                    <Link className="block w-full" href={"/settings"} as="button" prefetch>
-                        <Settings className="mr-2" />
-                        Settings
-                    </Link>
-                </DropdownMenuItem>
-            </DropdownMenuGroup>
-            <DropdownMenuSeparator />
-            <DropdownMenuItem asChild>
-                <Link className="block w-full"  href={"/"} as="button" onClick={signOutAction}>
-                    <LogOut className="mr-2" />
-                    Log out
-                </Link>
-            </DropdownMenuItem>
-        </>
-    );
+export function UserMenuContent({ user }: { user: User }) {
+  return (
+    <>
+      <DropdownMenuLabel className="p-0 font-normal">
+        <div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+          <UserInfo user={user} showEmail={true} />
+        </div>
+      </DropdownMenuLabel>
+      <DropdownMenuSeparator />
+      <DropdownMenuGroup>
+        <DropdownMenuItem asChild>
+          <Link className="block w-full" href={"/settings"} prefetch>
+            <Settings className="mr-2" />
+            Settings
+          </Link>
+        </DropdownMenuItem>
+      </DropdownMenuGroup>
+      <DropdownMenuSeparator />
+      <DropdownMenuItem onClick={signOutAction}>
+        <LogOut className="mr-2" />
+        Log out
+      </DropdownMenuItem>
+    </>
+  );
 }
